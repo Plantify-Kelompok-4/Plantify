457e5a46beb45d76d0634cb025fe9770126ddb89
MawarKusumaPutri <mawarkusumaputri4@gmail.com>
2025-01-04 09:56:02 +0700

    Fitur Konsultasi, Lokasi & Search Lokasi
    Individual

--- WADxManprosi/resources/views/filament/widgets/order-count-widget.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="bg-white rounded-lg p-4 border border-gray-300">
-    <h3>Jumlah Pemesanan</h3>
-    <p>{{ $this->getOrderCount() }}</p>
-</div>
--- WADxManprosi/resources/views/filament/widgets/user-count-widget.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="bg-white rounded-lg p-4 border border-gray-300">
-    <h3>Jumlah Pengguna</h3>
-    <p>{{ $this->getUserCount() }}</p>
-</div> 
